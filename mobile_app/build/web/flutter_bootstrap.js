6456b21d985c643db87496612415c7d064670fba
Saurabh <saurabhkr1825@gmail.com>
2026-04-02 22:34:16 +0530

    Update web app title, metadata, and logos

--- mobile_app/build/web/flutter_bootstrap.js
+++ mobile_app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1731078191" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1602596085" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
